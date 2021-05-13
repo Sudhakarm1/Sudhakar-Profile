b69b0e32d79e5fe8c998749c437d7a45f2aa9d9f
Sudhakarm1 <51196945+Sudhakarm1@users.noreply.github.com>
2021-05-13 20:39:11 +0530

    First-Update

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validators, FormBuilder, AbstractControl } from '@angular/forms';
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  constructor(private fb:FormBuilder) { }
+  ImageSrc:string="./assets/Image/Cart.png";
+  ImageSrcHome:string="./assets/Image/PassPort size Photo.png";
+  Resume:string="./assets/Image/Java-1.png";
+  Angular:string="./assets/Image/angular-icon.svg";
+  Spring:string="./assets/Image/spring-boot.png";
+  JavaScript:string="./assets/Image/javascript.svg";
+  jQuery:string="./assets/Image/JQuery.png";
+  bootatrap:string="./assets/Image/Bootstrap.png";
+  Microsoft:string="./assets/Image/microsoft-sql-server.svg";
+  MySql:string="./assets/Image/MySQL.png";
+  Mongo:string="./assets/Image/mongodb.svg";
+  CSharp:string="./assets/Image/CSharp.png";
+  DotNet:string="./assets/Image/NET_Core.png";
+  DotNetMVC:string="./assets/Image/aspnet.png";
+  Html:string="./assets/Image/html.png";
+  Css:string="./assets/Image/CSS3.png";
+  python:string="./assets/Image/Python.png";
+  CertificationPro:string="./assets/Image/Certification-1.png";
+  CertificationGuvi:string="./assets/Image/Certification-2.jpg";
+  AboutImage:string="./assets/Image/3d-flame-100.png";
+  ngOnInit
+  (): void {
+  }
+  form = this.fb.group({
+    FirstName: ['',Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(10),Validators.pattern('^[a-zA-Z ]*$'),])],
+    LastName: ['',Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(10),Validators.pattern('^[a-zA-Z ]*$'),])],
+    Email: ['',Validators.compose([Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])],
+    Mobile: ['',Validators.compose([Validators.required, Validators.minLength(10),Validators.maxLength(12),Validators.pattern('^[0-9]*$'),])],
+    Subject:['',Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(30),Validators.pattern('^[a-zA-Z ]*$'),])]
+  });
+  OnSubmit():void
+  {
+    console.log("Successfully Submitted");
+  }
+  Click():void
+  {
+    
+  }
+}
